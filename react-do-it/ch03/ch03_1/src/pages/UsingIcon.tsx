0dfb825a643254b9316e5796e5832c702744a6b4
editorseok <profile.heeseok@gmail.com>
2023-12-11 23:33:51 +0900

    improve: Add Material-Icon Example

--- react-do-it/ch03/ch03_1/src/pages/UsingIcon.tsx
+++ react-do-it/ch03/ch03_1/src/pages/UsingIcon.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Icon } from '../components';
 
 const UsingIcon = () => {
-  return <div>UsingIcon</div>;
+  return (
+    <div>
+      <h3>UsingIcon</h3>
+      <Icon name="home" style={{ color: 'blue' }} />
+      <Icon
+        name="check_circle_outline"
+        style={{ fontSize: '50px', color: 'red' }}
+      />
+    </div>
+  );
 };
 
 export default UsingIcon;
