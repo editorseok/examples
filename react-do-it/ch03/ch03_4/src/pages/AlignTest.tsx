931b40532ce50aafac7b177da14480c45c924151
editorseok <profile.heeseok@gmail.com>
2024-01-02 23:09:16 +0900

    implement: Add a Flex example

---
+++ react-do-it/ch03/ch03_4/src/pages/AlignTest.tsx
@@ -0,0 +1,12 @@
+import { Title } from '../components';
+
+export default () => {
+  return (
+    <section className="mt-4">
+      <Title>CopyMe</Title>
+      <div className="mt-4"></div>
+    </section>
+  );
+};
+
+// export default AlignTest;
---
+++ react-do-it/ch03/ch03_4/src/pages/Card.tsx
@@ -0,0 +1,10 @@
+import { Title } from '../components';
+
+export default () => {
+  return (
+    <section className="mt-4">
+      <Title>CopyMe</Title>
+      <div className="mt-4"></div>
+    </section>
+  );
+};
