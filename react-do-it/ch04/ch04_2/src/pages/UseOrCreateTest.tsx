eacb25bce376474aa3776d10b7f9bb2069ec763a
editorseok <profile.heeseok@gmail.com>
2024-01-24 23:14:57 +0900

    improve: Add a hook example

---
+++ react-do-it/ch04/ch04_2/src/pages/UseOrCreateTest.tsx
@@ -0,0 +1,46 @@
+import { Avatar, Title } from '../components';
+import { useOrCreate } from './useOrCreate';
+import * as D from '../data';
+
+const UseOrCreateTest = () => {
+  const headTexts = useOrCreate<string[]>('headTexts', () => [
+    'No.',
+    'Name',
+    'Job Title',
+    'Email Adress',
+  ]);
+  const users = useOrCreate<D.IUser[]>('users', () =>
+    D.makeArray(100).map(D.makeRandomUser),
+  );
+  const head = useOrCreate('head', () =>
+    headTexts.map(text => <th key={text}>{text}</th>),
+  );
+  const body = useOrCreate('children', () =>
+    users.map((user, index) => (
+      <tr key={user.uuid}>
+        <th>{index + 1}</th>
+        <td className="flex items-center">
+          <Avatar src={user.avatar} size="1.5rem" />
+          <p className="ml-2">{user.name}</p>
+        </td>
+        <td>{user.jobTitle}</td>
+        <td>{user.email}</td>
+      </tr>
+    )),
+  );
+  return (
+    <div className="mt-4">
+      <Title>CreateOrUseTest</Title>
+      <div className="p-4 mt-4 overflow-x-auto">
+        <table className="table w-full table-zebra table-compact">
+          <thead>
+            <tr>{head}</tr>
+          </thead>
+          <tbody>{body}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UseOrCreateTest;
